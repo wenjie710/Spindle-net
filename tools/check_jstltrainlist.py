672de7109b98047c0ea26fc1670e2f8b4e58dca4
Wenjie <31196397+wenjie710@users.noreply.github.com>
2017-08-23 13:58:39 +0800

    Update check_jstltrainlist.py

--- tools/check_jstltrainlist.py
+++ tools/check_jstltrainlist.py
@@ -1,7 +1,11 @@
 import numpy as np
 from argparse import ArgumentParser
 from collections import defaultdict
+import os.path as osp
 
+root = osp.join(osp.dirname(osp.abspath(__file__)), '..')
+if root not in sys.path:
+    sys.path.insert(0, root)
 from utils import *
 
 
